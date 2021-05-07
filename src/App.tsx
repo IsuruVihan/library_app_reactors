59da0807f1586531e55558118af0346ec4da73d2
IsuruVihan <isuruvihan@gmail.com>
2021-05-07 12:51:27 +0530

    Books and Authors headings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React, {FC} from 'react';
+import React, {FC, Fragment} from 'react';
 import './App.scss';
 import {Col, Container, Row} from "react-bootstrap";
 import libraryImg from './assets/images/anna-hunko-ajE5goOGzZc-unsplash.jpg';
+import Books from "./components/Books/Books";
+import Authors from "./components/Authors/Authors";
 
 const App: FC = () => {
-  return(
-      <Container className="App px-0" fluid>
-        <h1 className="text-center py-2">My Library</h1>
-        <img src={libraryImg} alt="Library" />
-        <Row>
-            <Col xs={9}></Col>
-            <Col xs={3}>
-                Photo by
-                <a href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                Anna Hunko</a>
-                on
-                <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                    Unsplash
-                </a>
-            </Col>
-        </Row>
-      </Container>
+    return(
+        <Fragment>
+            <Container className="App px-0" fluid={true}>
+                <h1 className="text-center py-2">My Library</h1>
+                <img src={libraryImg} alt="Library" />
+            </Container>
+            <Container className="credits">
+                <Row>
+                    <Col xs={9} />
+                    <Col className="text-end photo-credits">
+                        Photo by
+                        <a href="https://unsplash.com/@annahunko?
+                        utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                        >
+                        Anna Hunko</a>
+                        on
+                        <a href="https://unsplash.com/?
+                        utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                        >
+                            Unsplash
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid>
+                <Row>
+                    <Books />
+                    <Authors />
+                </Row>
+            </Container>
+        </Fragment>
   );
 }
 
