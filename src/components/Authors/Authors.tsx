064545a68c2a8535d1acc32ddd0e7a26f2f22c7b
IsuruVihan <isuruvihan@gmail.com>
2021-05-08 23:21:39 +0530

    Author UX (full) completed

--- src/components/Authors/Authors.tsx
+++ src/components/Authors/Authors.tsx
@@ -1,19 +1,46 @@
 import React, {FC} from 'react';
-import {Col} from "react-bootstrap";
+import {Row, Col} from "react-bootstrap";
 import '../../assets/styles/Authors.scss';
-import { Plus } from 'react-feather';
+import AddAuthorForm from "./AddAuthorForm";
+import { Plus, Trash2, Edit } from 'react-feather';
 
 const Authors: FC = () => {
     return(
         <Col className="Authors px-5">
             <p className="title">Authors</p>
-            <span className="no-authors"><i>No authors listed here</i></span>
+            {/*<span className="no-authors"><i>No authors listed here</i></span>*/}
+            <ul className="author-list px-0">
+                <li>
+                    <Row>
+                        <Col xs={10}>1. Author 1</Col>
+                        <Col><Edit className="edit-btn" /></Col>
+                        <Col><Trash2 className="delete-btn" /></Col>
+                    </Row>
+                </li>
+                <li>
+                    <Row>
+                        <Col xs={10}>2. Author 2</Col>
+                        <Col><Edit className="edit-btn" /></Col>
+                        <Col><Trash2 className="delete-btn" /></Col>
+                    </Row>
+                </li>
+                <li>
+                    <Row>
+                        <Col xs={10}>3. Author 3</Col>
+                        <Col><Edit className="edit-btn" /></Col>
+                        <Col><Trash2 className="delete-btn" /></Col>
+                    </Row>
+                </li>
+            </ul>
             <p className="add-author pt-3">
                 <Plus color="#034fa5" className="plus" />
                 <span className="add-author-text">
-                    Add Author
+                    Add Book
                 </span>
             </p>
+            <div>
+                <AddAuthorForm />
+            </div>
         </Col>
     );
 }
