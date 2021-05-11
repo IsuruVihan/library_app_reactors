0cfe28670332f8d52949b9af540965bc90521712
IsuruVihan <isuruvihan@gmail.com>
2021-05-11 19:42:54 +0530

    Re-arranged Authors.tsx with Containers, Rows, and Columns

--- src/components/Authors/Authors.tsx
+++ src/components/Authors/Authors.tsx
@@ -1,47 +1,94 @@
 import React, {FC} from 'react';
-import {Row, Col} from "react-bootstrap";
+import {Row, Col, Container} from "react-bootstrap";
 import '../../assets/styles/partials/Authors.scss';
 import AddAuthorForm from "./AddAuthorForm";
 import { Plus, Trash2, Edit } from 'react-feather';
 
 const Authors: FC = () => {
     return(
-        <Col className="Authors">
-            <p className="title">Authors</p>
-            {/*<span className="no-authors"><i>No authors listed here</i></span>*/}
-            <ul className="author-list px-0">
-                <li>
-                    <Row>
-                        <Col xs={10}>1. Author 1</Col>
-                        <Col><Edit className="edit-btn" /></Col>
-                        <Col><Trash2 className="delete-btn" /></Col>
-                    </Row>
-                </li>
-                <li>
-                    <Row>
-                        <Col xs={10}>2. Author 2</Col>
-                        <Col><Edit className="edit-btn" /></Col>
-                        <Col><Trash2 className="delete-btn" /></Col>
-                    </Row>
-                </li>
-                <li>
-                    <Row>
-                        <Col xs={10}>3. Author 3</Col>
-                        <Col><Edit className="edit-btn" /></Col>
-                        <Col><Trash2 className="delete-btn" /></Col>
-                    </Row>
-                </li>
-            </ul>
-            <p className="add-author pt-3">
-                <Plus color="#034fa5" className="plus" />
-                <span className="add-author-text">
-                    Add Book
-                </span>
-            </p>
-            <div>
-                <AddAuthorForm />
-            </div>
-        </Col>
+        <Container fluid>
+            <Row className="Authors">
+                <Col xs={12}>
+                    <p className="title">Authors</p>
+                </Col>
+                {/*<Col xs={12}>*/}
+                {/*    <p className="no-authors"><i>No authors listed here</i></p>*/}
+                {/*</Col>*/}
+                <Col xs={12}>
+                    <ul className="author-list px-0">
+                        <li>
+                            <Row>
+                                <Col xs={10}>1. Author 1</Col>
+                                <Col><Edit className="edit-btn" /></Col>
+                                <Col><Trash2 className="delete-btn" /></Col>
+                            </Row>
+                        </li>
+                        <li>
+                            <Row>
+                                <Col xs={10}>2. Author 2</Col>
+                                <Col><Edit className="edit-btn" /></Col>
+                                <Col><Trash2 className="delete-btn" /></Col>
+                            </Row>
+                        </li>
+                        <li>
+                            <Row>
+                                <Col xs={10}>3. Author 3</Col>
+                                <Col><Edit className="edit-btn" /></Col>
+                                <Col><Trash2 className="delete-btn" /></Col>
+                            </Row>
+                        </li>
+                    </ul>
+                </Col>
+                <Col xs={12}>
+                    <p className="add-author pt-3">
+                        <Plus color="#034fa5" className="plus" />
+                        <span className="add-author-text">
+                            Add Author
+                        </span>
+                    </p>
+                </Col>
+                <Col xs={12}>
+                    <AddAuthorForm />
+                </Col>
+            </Row>
+
+            {/*<Col className="Authors">*/}
+            {/*    <p className="title">Authors</p>*/}
+            {/*    /!*<span className="no-authors"><i>No authors listed here</i></span>*!/*/}
+            {/*    <ul className="author-list px-0">*/}
+            {/*        <li>*/}
+            {/*            <Row>*/}
+            {/*                <Col xs={10}>1. Author 1</Col>*/}
+            {/*                <Col><Edit className="edit-btn" /></Col>*/}
+            {/*                <Col><Trash2 className="delete-btn" /></Col>*/}
+            {/*            </Row>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <Row>*/}
+            {/*                <Col xs={10}>2. Author 2</Col>*/}
+            {/*                <Col><Edit className="edit-btn" /></Col>*/}
+            {/*                <Col><Trash2 className="delete-btn" /></Col>*/}
+            {/*            </Row>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <Row>*/}
+            {/*                <Col xs={10}>3. Author 3</Col>*/}
+            {/*                <Col><Edit className="edit-btn" /></Col>*/}
+            {/*                <Col><Trash2 className="delete-btn" /></Col>*/}
+            {/*            </Row>*/}
+            {/*        </li>*/}
+            {/*    </ul>*/}
+            {/*    <p className="add-author pt-3">*/}
+            {/*        <Plus color="#034fa5" className="plus" />*/}
+            {/*        <span className="add-author-text">*/}
+            {/*            Add Book*/}
+            {/*        </span>*/}
+            {/*    </p>*/}
+            {/*    <div>*/}
+            {/*        <AddAuthorForm />*/}
+            {/*    </div>*/}
+            {/*</Col>*/}
+        </Container>
     );
 }
 
