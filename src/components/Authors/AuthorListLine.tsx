6009011a39c7ea57768af113f841f0c7d530f0e1
IsuruVihan <isuruvihan@gmail.com>
2021-05-14 13:25:07 +0530

    Edit author overlay added. (Need to fix the position)

--- src/components/Authors/AuthorListLine.tsx
+++ src/components/Authors/AuthorListLine.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {Col, Row} from "react-bootstrap";
+import {Col, OverlayTrigger, Row, Tooltip} from "react-bootstrap";
 import {Edit, Trash2} from "react-feather";
 import '../../assets/styles/partials/AuthorListLine.scss';
 
@@ -11,12 +11,32 @@ type AuthorListLineProps = {
 };
 
 const AuthorListLine: FC<AuthorListLineProps> = (props) => {
+    const showEditOverlay = () => (
+        <Tooltip id="button-tooltip">
+            Edit author name
+        </Tooltip>
+    );
+
     return(
         <li>
             <Row>
-                <Col xs={10}>{props.id}. {props.name}</Col>
-                <Col><Edit className="edit-btn" onClick={() => props.updateRequest(props.id)} /></Col>
-                <Col><Trash2 className="delete-btn" onClick={() => props.delete(props.id)} /></Col>
+                <Col xs={10}>
+                    {props.id}. {props.name}
+                </Col>
+                <Col>
+                    <OverlayTrigger
+                        trigger="hover"
+                        key="right"
+                        placement="right"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={showEditOverlay}
+                    >
+                        <Edit className="edit-btn" onClick={() => props.updateRequest(props.id)} />
+                    </OverlayTrigger>
+                </Col>
+                <Col>
+                    <Trash2 className="delete-btn" onClick={() => props.delete(props.id)} />
+                </Col>
             </Row>
         </li>
     );
