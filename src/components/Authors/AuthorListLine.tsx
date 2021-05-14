76741e93b86671967076db23fd61638ad77e35d5
IsuruVihan <isuruvihan@gmail.com>
2021-05-14 14:16:24 +0530

    Delete author confirmation modal done

--- src/components/Authors/AuthorListLine.tsx
+++ src/components/Authors/AuthorListLine.tsx
@@ -1,7 +1,8 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Col, Row} from "react-bootstrap";
 import {Edit, Trash2} from "react-feather";
 import '../../assets/styles/partials/AuthorListLine.scss';
+import DeleteAuthorModal from "./DeleteAuthorModal";
 
 type AuthorListLineProps = {
     name: string,
@@ -11,13 +12,37 @@ type AuthorListLineProps = {
 };
 
 const AuthorListLine: FC<AuthorListLineProps> = (props) => {
+    // Visibility of delete author modal
+    const [showDeleteAuthorModal, setShowDeleteAuthorModal] = useState<boolean>(false);
+
+    // Display delete author modal
+    const openDeleteAuthorModal = () => {
+        setShowDeleteAuthorModal(true);
+    }
+    // When click 'Close'
+    const refuseDeleteAuthorAction = () => {
+        setShowDeleteAuthorModal(false);
+    }
+    // When click 'Yes, Delete'
+    const acceptDeleteAuthorAction = () => {
+        props.delete(props.id);
+        setShowDeleteAuthorModal(false);
+    }
+
     return(
         <li>
             <Row>
                 <Col xs={10}>{props.id}. {props.name}</Col>
                 <Col><Edit className="edit-btn" onClick={() => props.updateRequest(props.id)} /></Col>
-                <Col><Trash2 className="delete-btn" onClick={() => props.delete(props.id)} /></Col>
+                <Col><Trash2 className="delete-btn" onClick={() => openDeleteAuthorModal()} /></Col>
             </Row>
+
+            <DeleteAuthorModal
+                authorToDelete={props.name}
+                isVisible={showDeleteAuthorModal}
+                closeModal={refuseDeleteAuthorAction}
+                acceptDeleteAction={acceptDeleteAuthorAction}
+            />
         </li>
     );
 }
